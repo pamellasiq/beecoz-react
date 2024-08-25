a6eb907cad964e09edb71c15387ef14960a53018
Pamella Siqueira <contatopamellasiqueira@gmail.com>
2024-08-24 21:49:39 -0300

    Create login page responsive

---
+++ beecoz/src/assets/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.modules.css"
+
+function Header() {
+    return (
+            <div className="header">
+            <img src="bee.png" className="logo" alt="Logo da beecoz" />
+            </div>
+    );
+}
+
+export default Header;
---
+++ beecoz/src/assets/Components/Login/Login.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Login.modules.css"
+import Switch from "../Switch/Switch";
+
+function Login() {
+    return (
+            <div className="login">
+                <form className="form-login">
+                    <div className="input-login">
+                    <i className="ri-user-line"></i><input type="text" placeholder="Seu Login" />
+                    </div>
+                    <div className="input-login">
+                    <i className="ri-lock-password-line"></i><input type="password" placeholder="Sua Senha" />
+                    </div>
+
+                    <div className="remember-login">
+                    <div className="remember-me">
+                    <Switch/>
+                    <p>Lembre de mim</p>
+                    </div>
+                    <a>Esqueceu a senha?</a>
+                    </div>
+
+                    <button className="btn-login">Login</button>
+                    <div className="icons">
+                    <button className="social"><i className="ri-facebook-fill"></i>Facebook</button>
+                    <button className="social"><i className="ri-google-fill"></i>Google</button>
+                    </div>
+                </form>
+                <div className="register">
+                <p>Não tem uma conta? <a>Cadastre-se!</a></p>
+                </div>
+            </div>
+    );
+}
+
+export default Login;
